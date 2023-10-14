8fad41b492613145b32a6f85db0fb0ded76b145c
Aman Ali <itsamanali5@gmail.com>
2023-10-14 19:50:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+       hello
+       <div className="head"> 
+          <img src="./typecat.png" alt="" />
+          <h3>login</h3> 
+       </div>
+       <div className="typeBox">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas voluptates placeat quos molestiae ducimus autem esse dicta adipisci ab consequuntur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium atque recusandae amet voluptate hic explicabo corrupti blanditiis provident quam autem ipsa maiores fugiat nulla, labore iure nisi? Debitis quibusdam, eos ipsam autem facere accusantium numquam quam dolorem culpa voluptatem error voluptates officia ea perspiciatis illum voluptatibus eaque dicta commodi cum!</p>
+       </div>
     </div>
   );
 }
